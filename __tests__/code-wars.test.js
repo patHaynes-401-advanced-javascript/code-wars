af73cf51b95420ac3d2f7f3293c4cd558316ac72
Pat Haynes <pathaynes32@gmail.com>
2019-09-29 16:57:21 -0700

    another one down

--- __tests__/code-wars.test.js
+++ __tests__/code-wars.test.js
@@ -1,7 +1,8 @@
 const { 
   mask, 
-  highAndLow 
-} = require('../lib/masked-string');
+  highAndLow,
+  findTheNonRepeating 
+} = require('../lib/code-wars');
 
 
 describe('code challenges', () => {
@@ -16,5 +17,10 @@ describe('code challenges', () => {
     expect(highAndLow('1 2 3 4 5')).toBe('5 1');
     expect(highAndLow('1 2 -3 4 5')).toBe('5 -3');
     expect(highAndLow('1 9 3 4 -5')).toBe('9 -5');
+  });
+  it('returns the first non-repeating number', () => {
+    expect(findTheNonRepeating('1212121231212')).toBe('3');
+    expect(findTheNonRepeating('43222344347223')).toBe('7');
+    expect(findTheNonRepeating('4565464564562554668')).toBe('2');
   });
 });
